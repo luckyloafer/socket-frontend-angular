0f38016706d10014e25869909168802de11fef4e
luckyloafer <patnanalakshmanamurthy9@gmail.com>
2024-09-22 10:08:19 +0530

    socket established commit 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { io } from 'socket.io-client';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+  socket: any;
+  messages: string[] = [];
+  newMessage = '';
+
+  ngOnInit() {
+    // Connect to the backend socket.io server
+    console.log('initied')
+    try {
+      console.log("inside");
+      
+      this.socket = io('https://backend-ny0k.onrender.com/', {
+        transports: ['websocket', 'polling'],  // Allow WebSocket and fallback to polling
+        upgrade: true  // Allow Socket.IO to try upgrading from polling to WebSocket
+      });
+    } catch (error) {
+      console.log(error)
+    }
+    
+    // Listen for messages from the server
+    this.socket.on('message', (message: string) => {
+      this.messages.push(message);
+    });
+  }
+  sendMessage() {
+    if (this.newMessage.trim()) {
+      // Emit the message to the server
+      
+      this.socket.emit('message', this.newMessage);
+      this.newMessage = '';
+    }
+  }
 }
